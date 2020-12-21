aa13df4a273f7ce061dd7e01fbf7acbcb00db89c
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-12-21 04:13:15 +0100

    updated converter.js component

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from 'react'
 import Form from './middlewareComps/Form'
 
@@ -37,63 +36,4 @@ function Converter({formInput}) {
 }
 
 export default Converter
-=======
-import React, { useState } from "react";
-import "../App.css";
-import DecimalTo from "./DecimalTo";
-import { TextField, Select, MenuItem, InputLabel } from "@material-ui/core";
 
-function changeToDecimal(value, type) {
-  // -> Number
-  //Helper Function that change Binary , Octal, Decimal , Hexadecimal to Decimal
-
-  switch (type) {
-    case "Binary":
-      return parseInt(value, 2).toString(10);
-    case "Octal":
-      return parseInt(value, 8).toString(10);
-    case "Decimal":
-      return value;
-    case "Hexadecimal":
-      return parseInt(value, 16).toString(10);
-  }
-}
-
-export default function Converter(props) {
-  // We convert the user input to Decimal  and then outputs the respective result ;
-  const [inputValue, setInputValue] = useState(0);
-  const [inputType, setInputType] = useState("Decimal");
-
-  function handleInputTypeChange(e) {
-    setInputType(e.target.value);
-  }
-  function handleInputValueChange(e) {
-    setInputValue(e.target.value);
-  }
-  return (
-    <div className="converter">
-      <div className="converter__result">
-        <DecimalTo value={changeToDecimal(inputValue, inputType)} />
-      </div>
-      <form className="converter__input">
-        <TextField
-          id="standard-basic"
-          label="Enter the Number Here "
-          value={inputValue}
-          onChange={handleInputValueChange}
-        />
-        <Select
-          value={inputType}
-          onChange={handleInputTypeChange}
-          label="type"
-          className="input-type"
-        >
-          <MenuItem value={"Decimal"}> Decimal</MenuItem>
-          <MenuItem value={"Binary"}>Binary </MenuItem>
-          <MenuItem value={"Hexadecimal"}> HexaDecimal</MenuItem>
-        </Select>
-      </form>
-    </div>
-  );
-}
->>>>>>> 17f3e1881fb0d4c12a47281b8cfeefc0da887587
