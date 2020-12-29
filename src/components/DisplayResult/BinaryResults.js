f032378b55539c4bd6f8dec9d3df554aaef7c51c
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-12-29 20:19:07 +0100

    added materialUI comps for binary results

--- src/components/DisplayResult/BinaryResults.js
+++ src/components/DisplayResult/BinaryResults.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { Box, Typography, Grid } from '@material-ui/core'
 
-const BinaryTo =({result1, result2, result3}) => {
-    return (
-        <div>
-          <p>result1 = {result1} </p>
-          <p>result2 = {result2}</p>
-          <p>result3 = {result3}</p>
-        </div> 
-    )
+const BinaryTo = ({ result1, result2, result3 }) => {
+  return (
+    <Grid container xs={12} md={7} spacing={3}>
+      <Grid item xs={12} sm={12}>
+        <Typography variant="h6" >
+          Result In Decimal
+        </Typography>
+        <Box color="text.secondary" borderColor="grey.500" border={1} marginBottom="20px" padding="10px">
+          <Typography >
+            {result1}
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} sm={12}>
+        <Typography variant="h6">
+          Result in Octal
+        </Typography>
+        <Box color="text.secondary" borderColor="grey.500" border={1} marginBottom="20px" padding="10px">
+          <Typography >
+            {result2}
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} sm={12}>
+        <Typography variant="h6" >
+          Result in Hexadecimal
+        </Typography>
+        <Box color="text.secondary" borderColor="grey.500" border={1} marginBottom="20px" padding="10px">
+          <Typography >
+            {result3}
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+
+  )
 }
 
 export default BinaryTo
