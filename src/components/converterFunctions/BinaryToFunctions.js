a65f554d31e23c9b66300dea76c3930df7b883b8
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-12-29 20:30:06 +0100

    fix return NaN logical and remove condition

--- src/components/converterFunctions/BinaryToFunctions.js
+++ src/components/converterFunctions/BinaryToFunctions.js
@@ -12,13 +12,13 @@ const BinaryToFunctions =({value}) => {
         let result = parseInt(binValue, 2).toString(8)
         return isNaN(result) ? 0: result
     }
-    const DecToHexadecimal = (binValue=0) =>{
+    const BinToHexadecimal = (binValue=0) =>{
         let result = parseInt(binValue, 2).toString(16)
-        return isNaN(result) ? 0: result
+        return result
     }
     let result1 = BinToDecimal(value)
     let result2 = BinToOctal(value)
-    let result3 = DecToHexadecimal(value)
+    let result3 = BinToHexadecimal(value)
     
     
     return (
