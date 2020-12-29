d8ed10de6401abf9d9115b155b7cb37776122401
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-12-29 20:21:00 +0100

    added materialUI comps for decimal results

--- src/components/DisplayResult/DecimalResults.js
+++ src/components/DisplayResult/DecimalResults.js
@@ -1,18 +1,45 @@
 import React from 'react'
+import { Box, Typography, Grid } from '@material-ui/core'
 
-const DecimalTo =({result1, result2, result3}) => {
-    // console.log(props)
-    return (
-        <div>
-          Jackson says hello
-          <p>Binary Result</p>
-          <div>{result1}</div>
-          <p>Octal Result</p>
-          <div>{result2}</div>
-          <p>Hexadecimal Result</p>
-          <div>{result3}</div>
-        </div>
-    )
+const DecimalTo = ({ result1, result2, result3 }) => {
+  // console.log(props)
+  return (
+    <Grid container xs={12} md={7} spacing={3}>
+      <Grid item xs={12} sm={12}>
+        <Typography variant="h6" >
+          Result In Binary
+        </Typography>
+        <Box color="text.secondary" borderColor="grey.500" border={1} marginBottom="20px" padding="10px">
+          <Typography >
+            {result1}
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} sm={12}>
+        <Typography variant="h6">
+          Result in Octal
+        </Typography>
+        <Box color="text.secondary" borderColor="grey.500" border={1} marginBottom="20px" padding="10px">
+          <Typography >
+            {result2}
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} sm={12}>
+        <Typography variant="h6" >
+          Result in Hexadecimal
+        </Typography>
+        <Box color="text.secondary" borderColor="grey.500" border={1} marginBottom="20px" padding="10px">
+          <Typography >
+            {result3}
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+
+  )
 }
 
 export default DecimalTo
