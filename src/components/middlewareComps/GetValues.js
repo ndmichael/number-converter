7c5f71cec75a19adf849b1ac16b92646e2319d4b
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-01-03 13:38:30 +0100

    fixing the ui/ux

--- src/components/middlewareComps/GetValues.js
+++ src/components/middlewareComps/GetValues.js
@@ -13,15 +13,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
-  }));
+}));
 
 const GetValues = () => {
     const classes = useStyles();
@@ -60,46 +60,46 @@ const GetValues = () => {
     return (
         <React.Fragment>
 
-        <div className={classes.root}>
-      <AppBar color="secondary" position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            SimpleNumber Converter
-          </Typography>
-          <Button color="inherit">Github</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+            <div className={classes.root}>
+                <AppBar color="inherit" position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            SimpleNumber Converter
+                        </Typography>
+                        <Button color="inherit">Github</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
 
-        <Grid container sm={12} spacing={2} >
-            <Grid item sm={2}></Grid>
-            <Grid item xs={12} sm={8} justify="center">
-                <Box m={4} p={4} style={{ boxShadow: "0 2px 7px rgba(0, 0, 0, 0.23)" }}>
-                {cond}
-                <Converter formInput={formInput} />
-                </Box>
+            <Grid container sm={12} spacing={2} >
+                <Grid item sm={2}></Grid>
+                <Grid item xs={12} sm={8} justify="center">
+                    <Box m={4} p={4} style={{ boxShadow: "0 2px 7px rgba(0, 0, 0, 0.23)" }}>
+                        {cond}
+                        <Converter formInput={formInput} />
+                    </Box>
+                </Grid>
+                <Grid item sm={2}></Grid>
             </Grid>
-            <Grid item sm={2}></Grid>
-        </Grid>
 
-        <Grid container>
-            <Grid item xs={12}>
-                <Box style={{ backgroundColor: "#dee2e6" }} p={2} align="center">
-                    <Typography variant="h6" color="textSecondary">
-                        Designed with react and materialUI
+            <Grid container>
+                <Grid item xs={12}>
+                    <Box style={{ backgroundColor: "#dee2e6" }} p={2} align="center">
+                        <Typography variant="h6" color="textSecondary">
+                            Designed with react and materialUI
                     </Typography>
-                    <Typography variant="subtitle">
-                        Easy and fun to use
+                        <Typography variant="subtitle">
+                            Easy and fun to use
                     </Typography>
-                    <Typography variant="subtitle2">
-                        &copy; copyright <Typography variant="span" color="secondary">MiiT</Typography> 
-                    </Typography>
-                </Box>
+                        <Typography variant="subtitle2">
+                            &copy; copyright <Typography variant="span" color="secondary">MiiT</Typography>
+                        </Typography>
+                    </Box>
+                </Grid>
             </Grid>
-        </Grid>
         </React.Fragment>
 
     )
